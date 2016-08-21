d92d8c1a272b9bbbb7d9de6187c781fea41b09f3
Kramer Campbell <kramer@kramerc.com>
2016-08-21 14:30:02 -0700

    Add access token reader and setup project

---
+++ FBConnect Protocol Handler/AppInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace FBConnect_Protocol_Handler
+{
+    [RunInstaller(true)]
+    public partial class AppInstaller : System.Configuration.Install.Installer
+    {
+        public AppInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            string execFile = Assembly.GetExecutingAssembly().Location;
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("Software\\Classes\\fbconnect\\shell\\open\\command");
+            key.SetValue("", "\"" + execFile + "\" \"%1\"");
+            key.Close();
+        }
+    }
+}
---
+++ FBConnect Protocol Handler/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Windows.Forms;
+
+namespace FBConnect_Protocol_Handler
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                Uri uri = new Uri(args[1]);
+                string fragment = uri.Fragment.Substring(1);
+                
+                txtAccessToken.Text = HttpUtility.ParseQueryString(fragment).Get("access_token");
+            }
+            else
+            {
+                MessageBox.Show("No URI was supplied", "FBConnect Protocol Handler");
+                Application.Exit();
+            }
+        }
+    }
+}
